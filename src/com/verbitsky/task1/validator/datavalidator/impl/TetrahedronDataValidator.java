a0924e5872933935c78704adbc8b15ef64367e05
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-08-22 14:50:08 +0300

    add regex for file lines validator

--- src/com/verbitsky/task1/validator/datavalidator/impl/TetrahedronDataValidator.java
+++ src/com/verbitsky/task1/validator/datavalidator/impl/TetrahedronDataValidator.java
@@ -3,13 +3,11 @@ package com.verbitsky.task1.validator.datavalidator.impl;
 import com.verbitsky.task1.validator.datavalidator.FigureDataValidator;
 
 public class TetrahedronDataValidator implements FigureDataValidator {
-//todo Написать регулярку
-    private static final String COORDINATE_REGEX = "111";
-    //private static final int CORRECT_COORDINATES_COUNT = 12;
+    private static final String CORRECT_FILE_LINE_WITH_COORDINATES = "(\\s*((\\d+?\\.\\d{1,2})\\s*){3}\\s*?;){4}";
 
     @Override
     public boolean validateData(String line) {
-        boolean result = line.matches(COORDINATE_REGEX);
+        boolean result = line.matches(CORRECT_FILE_LINE_WITH_COORDINATES);
         if (!result) {
             //todo write log
         }
