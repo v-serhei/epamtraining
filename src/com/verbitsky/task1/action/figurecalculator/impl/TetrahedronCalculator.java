f161dddad01f854495637435f5d7ce0886bc84f4
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-08-23 17:29:27 +0300

    add logging for TetrahedronCalculator.java

--- src/com/verbitsky/task1/action/figurecalculator/impl/TetrahedronCalculator.java
+++ src/com/verbitsky/task1/action/figurecalculator/impl/TetrahedronCalculator.java
@@ -5,10 +5,14 @@ import com.verbitsky.task1.entity.figure.Figure;
 import com.verbitsky.task1.entity.figure.impl.Tetrahedron;
 import com.verbitsky.task1.entity.point.AreaPoint;
 import com.verbitsky.task1.validator.figuretypevalidator.impl.TetrahedronTypeValidator;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static java.lang.Math.*;
 
 public class TetrahedronCalculator implements FigureCalculator {
+    private static Logger logger = LogManager.getLogger();
     private static final double ROUND_FACTOR = 100.0;
     private TetrahedronTypeValidator tetrahedronTypeValidator = new TetrahedronTypeValidator();
 
@@ -17,6 +21,7 @@ public class TetrahedronCalculator implements FigureCalculator {
         if (tetrahedronTypeValidator.isObjectFigure(figure)) {
             Tetrahedron tetrahedron = (Tetrahedron) figure;
             double res = pow(tetrahedron.getVergeSize(), 2) * sqrt(3);
+            logger.log(Level.INFO, "Calculating tetrahedron square: result = " + res);
             return roundResult(res);
         }
         return 0;
@@ -27,6 +32,7 @@ public class TetrahedronCalculator implements FigureCalculator {
         if (tetrahedronTypeValidator.isObjectFigure(figure)) {
             Tetrahedron tetrahedron = (Tetrahedron) figure;
             double res = (pow(tetrahedron.getVergeSize(), 3) * sqrt(2)) / 12;
+            logger.log(Level.INFO, "Calculating tetrahedron volume: result = " + res);
             return roundResult(res);
         }
         return 0;
@@ -34,6 +40,7 @@ public class TetrahedronCalculator implements FigureCalculator {
 
     @Override
     public double calculateVolumeRatio(Figure figure, double subspaceHeight) {
+        double result = 0.0;
         if (tetrahedronTypeValidator.isObjectFigure(figure)) {
             Tetrahedron tetrahedron = (Tetrahedron) figure;
             AreaPoint buf = tetrahedron.getPointA();
@@ -47,12 +54,13 @@ public class TetrahedronCalculator implements FigureCalculator {
                     new Tetrahedron(reducedAPoint, reducedBPoint, reducedCPoint, tetrahedron.getTopPoint()));
             double remainFigureVolume = fullTetrahedronVolume - reducedTetrahedronVolume;
             if (remainFigureVolume > reducedTetrahedronVolume) {
-                return roundResult(remainFigureVolume / reducedTetrahedronVolume);
+                result = roundResult(remainFigureVolume / reducedTetrahedronVolume);
             } else {
-                return roundResult(reducedTetrahedronVolume / remainFigureVolume);
+                result = roundResult(reducedTetrahedronVolume / remainFigureVolume);
             }
         }
-        return 0;
+        logger.log(Level.INFO, "Calculating tetrahedrons ration: result = " + result);
+        return result;
     }
 
     @Override
@@ -62,17 +70,20 @@ public class TetrahedronCalculator implements FigureCalculator {
             AreaPoint a = tetrahedron.getPointA();
             AreaPoint b = tetrahedron.getPointB();
             AreaPoint c = tetrahedron.getPointC();
-
             if (a.getXCoordinate() == 0 && b.getXCoordinate() == 0 && c.getXCoordinate() == 0) {
+                logger.log(Level.INFO, "Tetrahedrons is on YZ-coordinates plane");
                 return true;
             }
             if (a.getYCoordinate() == 0 && b.getYCoordinate() == 0 && c.getYCoordinate() == 0) {
+                logger.log(Level.INFO, "Tetrahedrons is on XZ-coordinates plane");
                 return true;
             }
             if (a.getZCoordinate() == 0 && b.getZCoordinate() == 0 && c.getZCoordinate() == 0) {
+                logger.log(Level.INFO, "Tetrahedrons is on XY-coordinates plane");
                 return true;
             }
         }
+        logger.log(Level.INFO, "Tetrahedrons not located on any coordinate plane");
         return false;
     }
 
