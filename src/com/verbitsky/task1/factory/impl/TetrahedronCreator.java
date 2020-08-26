85c16588819eca61799f14fcbf65f09c41a049c7
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-08-26 15:24:29 +0300

    fix creator

--- src/com/verbitsky/task1/factory/impl/TetrahedronCreator.java
+++ src/com/verbitsky/task1/factory/impl/TetrahedronCreator.java
@@ -10,10 +10,10 @@ import com.verbitsky.task1.validator.FigureCreationValidator;
 import com.verbitsky.task1.validator.FigureDataValidator;
 import com.verbitsky.task1.validator.impl.TetrahedronCreationValidator;
 import com.verbitsky.task1.validator.impl.TetrahedronDataValidator;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
